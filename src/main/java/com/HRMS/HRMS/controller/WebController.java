417a5a543aca8086a2b89bf7ee7c4fedf3564dce
muaz.zawawi <muaz.zawawi@imocha.com.my>
2025-08-08 16:33:16 +0800

    add hot reload to pom.xml, add login path to webController and add login.html

---
+++ src/main/java/com/HRMS/HRMS/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.HRMS.HRMS.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class WebController implements WebMvcConfigurer{
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("/user/login");
+    }
+}
